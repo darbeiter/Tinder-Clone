58a921e7722d34eebbeff26829651769ff177fad
iTzNightmare <dustin.arbeiter@gmx.de>
2023-10-26 01:13:21 +0200

    ✨ add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard';
+import Onboarding from './pages/Onboarding';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/onboarding" element={<Onboarding/>} />
+      </Routes>
+    </BrowserRouter>
+  
+
   );
 }
 
