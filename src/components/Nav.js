f85ea359ab6a50eaba9cc73e86b9f327a114fd25
iTzNightmare <dustin.arbeiter@gmx.de>
2023-10-26 01:45:59 +0200

    ✨finish basic stylish home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,15 @@
-export const Nav = () => {
-  return (
-    <div>Nav</div>
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/tinder_logo_color.png'
+
+
+export const Nav = ( {minimal, authToken} ) => {
+    return (
+    <nav>
+        <div className="logo-container">
+            <img className="logo" src={minimal ? colorLogo : whiteLogo} alt="Tinder Logo"/>
+        </div>
+        {!authToken && !minimal && <button className="nav-button">Log in</button>}
+    </nav>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,25 @@
 import Nav from '../components/Nav'
 
-const authToken = true
-
-const handleClick = () => {
-    console.log('clicked')
-}
 
 const Home = () => {
-  return (
-    <>
-        <Nav/>
-        <div className="home">
-            <h1>Swipe Right®</h1>
-            <button className="primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
-            </button>
+    const authToken = false
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+    return (
+        <>
+        <div className="overlay">
+            <Nav minimal={false} authToken={authToken}/>
+            <div className="home">
+                <h1>Swipe Right®</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+            </div>
         </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Home
