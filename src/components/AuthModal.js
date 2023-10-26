ad694ceb652027eb90e5227d413139ea42f9ffac
iTzNightmare <dustin.arbeiter@gmx.de>
2023-10-27 00:25:20 +0200

    🚧 auth modal design

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,10 +1,68 @@
-export const AuthModal = ({setShowModal}) => {
+import { useState } from "react"
+
+export const AuthModal = ({setShowModal, isSignUp}) => {
+    
+    const [ email, setEmail ] = useState(null)
+    const [ password, setPassword ] = useState(null)
+    const [ confirmPassword, setConfirmPassword ] = useState(null)
+    const [ error, setError ] = useState(null)
+
+    console.log(email, password, confirmPassword)
+
     const handleClick = () => {
         setShowModal(false)
     }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        try {
+            if(isSignUp && (password !== confirmPassword)) {
+                setError('Passwords need to match!')
+            }
+            console.log('make a POST request to our database')
+        } 
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+
     return (
-    <div>
-        <div onClick={handleClick}>⦻</div>
+    <div className="auth-modal">
+        <div className="close-icon" onClick={handleClick}>⦻</div>
+        <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+        <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                required={true}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+             <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                required={true}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+             {isSignUp && 
+             <input
+                type="password"
+                id="confirm-password"
+                name="confirm-password"
+                placeholder="confirm password"
+                required={true}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+            />} 
+            <input className="secondary-button" type="submit" value="submit"/>
+            <p>{error}</p>
+            <hr/>
+            <h2>GET THE APP</h2>
+        </form>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,17 @@ import whiteLogo from '../images/tinder_logo_white.png'
 import colorLogo from '../images/tinder_logo_color.png'
 
 
-export const Nav = ( {minimal, authToken} ) => {
+export const Nav = ( {minimal, authToken, setShowModal, showModal, setIsSignUp} ) => {
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
     return (
     <nav>
         <div className="logo-container">
             <img className="logo" src={minimal ? colorLogo : whiteLogo} alt="Tinder Logo"/>
         </div>
-        {!authToken && !minimal && <button className="nav-button">Log in</button>}
+        {!authToken && !minimal && <button className="nav-button" onClick={handleClick} disabled={showModal}>Log in</button>}
     </nav>
   )
 }
