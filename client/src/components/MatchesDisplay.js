12bdd06c7df06aea89d14fd49aff11ebe0e1e45e
iTzNightmare <dustin.arbeiter@gmx.de>
2023-10-31 18:55:15 +0100

    ✨ finishing matches functionality

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import { useCookies } from 'react-cookie'
+
 
 const MatchesDisplay = ({ matches, setClickedUser }) => {
 
   const [matchedProfiles, setMatchedProfiles] = useState(null)
+  const [cookies, setCookie, removeCookie] = useCookies(['token'])
+
+  
   const matchedUserIds = matches.map(({ user_id }) => user_id)
+  const userId = cookies.UserId
 
   const getMatches = async() => {
     try {
@@ -20,12 +26,15 @@ const MatchesDisplay = ({ matches, setClickedUser }) => {
 
   useEffect(() => {
     getMatches()
-  },[])
+  },[matches])
+
+
+  const filteredMatchedProfiles = matchedProfiles?.filter(matchedProfile => matchedProfile.matches.filter(profile => profile.user_id === userId).length > 0)
 
   return (
     <div className='matches-display'>
-      {matchedProfiles?.map((match, _index) => (
-        <div key={{_index}} className='match-card' onClick={() => setClickedUser(match)}>
+      {filteredMatchedProfiles?.map((match) => (
+        <div key={match.user_id} className='match-card' onClick={() => setClickedUser(match)}>
           <div className="img-container">
             <img src={match?.url} alt={match?.first_name + ' profile'}/>
           </div>
